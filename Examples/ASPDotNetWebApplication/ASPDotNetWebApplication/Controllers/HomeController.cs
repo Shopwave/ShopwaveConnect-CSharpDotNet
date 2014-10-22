055013c0e97864035f1a3d866acab4e970aa83a1
mithralaya <mithralaya@gmail.com>
2014-10-22 14:55:07 +0100

    files added

---
+++ Examples/ASPDotNetWebApplication/ASPDotNetWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+using System.Web.Script.Serialization;
+using ShopwaveConnect;
+
+namespace ASPDotNetWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        /* Your Shopwave ClientIdentifier (e.g. js7woa9ro028djsnakf778sn3wiam3ond274knao) */
+        public const string ClientIdentifier = "SHOPWAVE_CLIENT_IDENTIFIER";
+
+        /* Your Shopwave ClientSecret (e.g. 76h4389732ru2039r20ruju023r9u2309jk8sna0) */
+        public const string ClientSecret = "SHOPWAVE_CLIENT_SECRET";
+
+        /* Your Shopwave RedirectUri (e.g. http://my.app) */
+        public readonly Uri RedirectUri = new Uri("SHOPWAVE_REDIRECT_URI");
+
+        /* Your Shopwave Scope */
+        public readonly string[] Scope =  {"user","application", "merchant","store", "product", "category", "basket", "promotion", "log", "supplierStore", "supplier", "invoice", "stock"};
+
+        /* Example Shopwave API Objects */
+
+        public class Employee
+        {
+            public int roleId { get; set; }
+            public int merchantId { get; set; }
+        }
+
+        public new class User
+        {
+            public string id { get; set; }
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string email { get; set; }
+            public Employee employee { get; set; }
+        }
+
+        public class UserGet
+        {
+            public User user { get; set; }
+        }
+
+
+        public ActionResult Index()
+        {
+            string code = Request.QueryString["code"];
+            ShopwaveConnectManager connector = new ShopwaveConnectManager(ClientIdentifier, ClientSecret, RedirectUri, Scope);
+
+            /* Check for existing session */
+
+            if(code != null && code.Length > 0)
+            {
+                if (Session["Token"] == null)
+                {
+                    connector.AuthCode = code;
+
+                    /* Fetch access and refresh token */
+
+                    ShopwaveConnectManager.Token token = connector.MakeTokenCall();
+
+                    /* Store the token within the user session */ 
+
+                    Session["Token"] = token;
+
+                    return Redirect("/");
+                }
+            }
+
+            /* Check for existing session */
+
+            if (Session["Token"] != null)
+            {
+                ViewBag.LoggedIn = true;
+
+                Dictionary<string, string> headers = new Dictionary<string, string>()
+                {
+                    { "x-accept-version", "0.4"}
+                };
+
+                string responseString = connector.MakeShopwaveApiCall("user",(ShopwaveConnectManager.Token) Session["Token"], "GET", headers, null);
+
+                UserGet userGet = new JavaScriptSerializer().Deserialize<UserGet>(responseString);
+
+                ViewBag.User = userGet.user;
+            }
+            else
+            {
+                ViewBag.LoggedIn = false;
+                ViewBag.User = null;
+            }
+
+            return View();
+        }
+
+        public ActionResult RedirectAuth()
+        {
+            ShopwaveConnectManager connector = new ShopwaveConnectManager(ClientIdentifier, ClientSecret, RedirectUri, Scope);
+
+            /**
+             * As we are building a web app, we are redirecting the client to the auth URL.
+             * If you are building a desktop or windows phone application, you must create a WebView 
+             * and load the GetAuthoriseApplicationUri() on the the WebView where the user will be 
+             * presented with the login screen
+             */
+
+            return Redirect(connector.GetAuthoriseApplicationUri());
+        }
+
+        public ActionResult Logout()
+        {
+            ShopwaveConnectManager connector = new ShopwaveConnectManager(ClientIdentifier, ClientSecret, RedirectUri, Scope);
+
+            Session.Remove("Token");
+
+            /**
+             * As we are building a web app, we are redirecting the client to the logout URL.
+             * If you are building a desktop or windows phone application, you must create a WebView 
+             * and load the GetLogoutUri() on the the WebView where the user will be presented with 
+             * the login screen
+             */
+
+            return Redirect(connector.GetLogoutUri());
+        }
+    }
+}
---
+++ Libraries/ShopwaveConnect/ShopwaveConnectManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace ShopwaveConnect
+{
+    public sealed class ShopwaveConnectManager
+    {
+        private const String OAuthBaseUrl = "http://secure.merchantstack.com/";
+        private const String ApiBaseUrl = "http://api.merchantstack.com/";
+        private const String AuthUri = "oauth/authorize";
+        private const String TokenUri = "oauth/token";
+        private const String LogoutUri = "logout";
+        private readonly String ClientIdentifier;
+        private readonly String ClientSecret;
+        private readonly Uri RedirectUrl;
+        private readonly String[] Scope;
+
+        public String AccessType;
+        public String ResponseType;
+
+        public String AuthCode;
+
+        public class Token
+        {
+            public string refresh_token { get; set; }
+            public string token_type { get; set; }
+            public int expires_in { get; set; }
+            public string access_token { get; set; }
+        }
+ 
+        public ShopwaveConnectManager(String clientIdentifier, String clientSecret, Uri redirectUrl, String[] scope)
+        {
+            this.ClientIdentifier = clientIdentifier;
+            this.ClientSecret = clientSecret;
+            this.RedirectUrl = redirectUrl;
+            this.Scope = scope;
+            this.AccessType = "online";
+            this.ResponseType = "code";
+        }
+
+        public string GetAuthoriseApplicationUri ()
+        {
+            return OAuthBaseUrl + AuthUri + "?access_type=" + this.AccessType + "&redirect_uri=" + this.RedirectUrl.ToString() + "&client_id=" + this.ClientIdentifier + "&scope=" + string.Join(",", this.Scope) + "&response_type=" + this.ResponseType;
+        }
+
+        public string GetLogoutUri()
+        {
+            return OAuthBaseUrl + LogoutUri + "?access_type=" + this.AccessType + "&redirect_uri=" + this.RedirectUrl.ToString() + "&client_id=" + this.ClientIdentifier + "&scope=" + string.Join(",", this.Scope) + "&response_type=" + this.ResponseType;
+        }
+
+        public Token MakeTokenCall()
+        {
+            String postData = "access_type=" + this.AccessType + "&redirect_uri=" + this.RedirectUrl.ToString() + "&client_id=" + this.ClientIdentifier + "&scope=" + string.Join(",", this.Scope) + "&client_secret=" + this.ClientSecret + "&code=" + this.AuthCode + "&grant_type=authorization_code";
+            
+            return new JavaScriptSerializer().Deserialize<Token>(MakeWebQuery(GetTokenUri(), "POST", null, null, null, postData));
+        }
+
+        public Token RefreshTokenCall(Token tokenVar)
+        {
+            String postData = "access_type=" + this.AccessType + "&redirect_uri=" + this.RedirectUrl.ToString() + "&client_id=" + this.ClientIdentifier + "&scope=" + string.Join(",", this.Scope) + "&client_secret=" + this.ClientSecret + "&refresh_token=" + tokenVar.refresh_token + "&grant_type=refresh_token";
+
+            return new JavaScriptSerializer().Deserialize<Token>(MakeWebQuery(GetTokenUri(), "POST", null, null, null, postData));
+        }
+
+        public String MakeShopwaveApiCall(String endpoint, Token tokens, string method, Dictionary<string, string> headers, string postBody)
+        {
+            return MakeWebQuery(GetApiEndpoint(endpoint), method, tokens.token_type, tokens.access_token, headers, postBody);
+        }
+
+        private Uri GetTokenUri()
+        {
+            return new Uri(OAuthBaseUrl + TokenUri);
+        }
+
+        private Uri GetApiEndpoint(String endpoint)
+        {
+            return new Uri(ApiBaseUrl + endpoint);
+        }
+
+        private string MakeWebQuery(Uri requestUrl, string method, string tokenType, string accessToken, Dictionary<string, string> headers, string postData)
+        {
+            WebRequest webRequest = WebRequest.Create(requestUrl);
+            webRequest.Method = method;
+            webRequest.Headers.Add("Accept-Tenant", "uk");
+            webRequest.Headers.Add("Accept-Language", "en-GB");
+            webRequest.Headers.Add("Accept-Charset", "utf-8");
+
+            if(headers != null)
+            {
+                foreach (var headerPair in headers)
+                {
+                    webRequest.Headers.Add(headerPair.Key, headerPair.Value);
+                }
+            }
+
+            if (accessToken != null && tokenType != null)
+            {
+                webRequest.Headers.Add("Authorization", tokenType + " " + accessToken);
+            }
+
+            Stream dataStream;
+            
+            if(method == "POST")
+            {
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData.ToString());
+
+                webRequest.ContentType = "application/x-www-form-urlencoded";
+                webRequest.ContentLength = byteArray.Length;
+
+                dataStream = webRequest.GetRequestStream();
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+            }
+
+            WebResponse response = webRequest.GetResponse();
+            dataStream = response.GetResponseStream();
+
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            response.Close();
+
+            return responseFromServer;
+        }
+     }
+}
